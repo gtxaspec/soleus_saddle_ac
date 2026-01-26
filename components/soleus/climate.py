3ee91aae5bc9642556d1dfc4bcd65f85f3ed0f8d
gtxaspec <gtxent@gmail.com>
2026-01-25 19:34:41 -0800

    Add support for heat configuration in climate.py

--- components/soleus/climate.py
+++ components/soleus/climate.py
@@ -4,10 +4,15 @@ import esphome.config_validation as cv
 
 AUTO_LOAD = ["climate_ir"]
 
+CONF_SUPPORTS_HEAT = "supports_heat"
+
 soleus_ns = cg.esphome_ns.namespace("soleus")
 SoleusClimate = soleus_ns.class_("SoleusClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(SoleusClimate)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(SoleusClimate).extend({
+    cv.Optional(CONF_SUPPORTS_HEAT, default=False): cv.boolean,
+})
 
 async def to_code(config):
     var = await climate_ir.new_climate_ir(config)
+    cg.add(var.set_supports_heat(config[CONF_SUPPORTS_HEAT]))
